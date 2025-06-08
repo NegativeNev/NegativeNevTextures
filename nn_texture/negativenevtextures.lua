ff70232619adac722ac4b9e7f7d706d349e8466a
NegativeNev <nom58332@gmail.com>
2025-06-08 22:50:20 +0900

    Add files via upload

---
+++ nn_texture/negativenevtextures.lua
@@ -0,0 +1,71 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: NegativeNev Personal Reskins
+--- MOD_ID: NevTextures
+--- PREFIX: NevTextures
+--- MOD_AUTHOR: [NegativeNev]
+--- MOD_DESCRIPTION: NegativeNev's personal face card reskin pack. And also if the (formerly) fellow Isaac modders are wondering about my whereabouts, tell them that I'm likely never returning.
+--- VERSION: 1.0
+
+SMODS.Atlas {
+    key = 'NevTextures_lc',
+    path = 'NegativeNevPersonal-8BitDeck.png',
+    px = 71,
+    py = 95,
+}
+
+SMODS.Atlas {
+    key = 'NevTextures_hc',
+    path = 'NegativeNevPersonal-8BitDeck_opt2.png',
+    px = 71,
+    py = 95,
+}
+
+SMODS.DeckSkin {
+    key = 'NevTextures_Hearts',
+    suit = 'Hearts',
+    loc_txt = 'Corrupt Yet Beautiful',
+    ranks = {'Jack', 'Queen', 'King', 'Ace'},
+    display_ranks = {'King', 'Queen', 'Jack'},
+    lc_atlas = 'NevTextures_lc',
+    hc_atlas = 'NevTextures_hc',
+    pos_style = 'deck',
+}
+
+SMODS.DeckSkin {
+    key = 'NevTextures_Clubs',
+    suit = 'Clubs',
+    loc_txt = 'Magic Tragic',
+    ranks = {'Jack', 'Queen', 'King', 'Ace'},
+    display_ranks = {'King', 'Queen', 'Jack'},
+    lc_atlas = 'NevTextures_lc',
+    hc_atlas = 'NevTextures_hc',
+    pos_style = 'deck',
+}
+
+SMODS.DeckSkin {
+    key = 'NevTextures_Diamonds',
+    suit = 'Diamonds',
+    loc_txt = 'For The Hell Of It',
+    ranks = {'Jack', 'Queen', 'King', 'Ace'},
+    display_ranks = {'King', 'Queen', 'Jack'},
+    lc_atlas = 'NevTextures_lc',
+    hc_atlas = 'NevTextures_hc',
+    pos_style = 'deck',
+}
+
+SMODS.DeckSkin {
+    key = 'NevTextures_Spades',
+    suit = 'Spades',
+    loc_txt = 'Quadrifolium',
+    ranks = {'Jack', 'Queen', 'King', 'Ace'},
+    display_ranks = {'King', 'Queen', 'Jack'},
+    lc_atlas = 'NevTextures_lc',
+    hc_atlas = 'NevTextures_hc',
+    pos_style = 'deck',
+}
+
+--Peeked "Deltarune Brainrot Face Cards" for the code because I have no idea how the thing fucking works
+
+--Wish Balala had Steam Workshop support. Unfortunately we'll have to do it the hard way since everyone is forced to go through all the wack crap required for modding. I'm sorry, y'all.
+--Bear in mind this does NOT imply that modders can shit on the end-users to their hearts' content. Yes, I'm talking about you JSG. Even if I'm returning I'm never using Repentogon. EVER. Just to spite you. Enegue was damn right about it.
+--(No offense to you Fly_6, but your fellow devs really piss me off. You still earn my respect.)
